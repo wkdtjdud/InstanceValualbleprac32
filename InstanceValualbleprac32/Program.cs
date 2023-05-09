b3fcc5fd2923c3e40173b4c6272c33b7a0a2d586
Seoyeoung Jang <s2130@e-mirim.hs.kr>
2023-05-09 11:10:44 +0900

    클래스 변수

--- InstanceValualbleprac32/Program.cs
+++ InstanceValualbleprac32/Program.cs
@@ -13,7 +13,14 @@ namespace InstanceVariableprac32
             Product product = new Product();
             product.name = "임영웅";
             product.price = 100;
-
         }
+
+        //C#스타일
+        Product product2 = new Product() { name = "고구마", price = 2100 };
+        Product product3 = new Product() { name = "감자", price = 2100 };
+        Product product4 = new Product() { name = "호박고구마", price = 2100 };
+
+        Product product5 = new Product() { name = "달달고구마", price = 2100 };
+
     }
 }
